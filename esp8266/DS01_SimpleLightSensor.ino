5e0cbe37f5c238b466422e40b7a5c6febece17f2
Suphaporn28 <63359817+Suphaporn28@users.noreply.github.com>
2020-04-08 22:58:20 +0700

    Update DS01_SimpleLightSensor.ino

--- esp8266/DS01_SimpleLightSensor.ino
+++ esp8266/DS01_SimpleLightSensor.ino
@@ -8,7 +8,7 @@ void setup(){
  Serial.begin(115200);
  pinMode(2,INPUT_PULLUP);
  delay(5000);
- Serial.println("program DS01");
+ Serial.println("program somsom");
  Serial.println("Simple Light Sensor");
  Serial.println("modified for esp8266"); 
  Serial.println("modified by Digital Siam");
